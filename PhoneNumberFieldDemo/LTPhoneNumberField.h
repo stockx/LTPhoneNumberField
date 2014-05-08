032975868ab7fd6c42cef57c15a6858431c1dcac
Colin Regan <colin@redcup.la>
2014-05-08 17:30:50 -0400

    Adds LTPhoneNumberField UITextField subclass

---
+++ PhoneNumberFieldDemo/LTPhoneNumberField.h
@@ -0,0 +1,13 @@
+//
+//  LTPhoneNumberField.h
+//  PhoneNumberFieldDemo
+//
+//  Created by Colin Regan on 5/8/14.
+//  Copyright (c) 2014 Lua Technologies, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTPhoneNumberField : UITextField
+
+@end
---
+++ PhoneNumberFieldDemo/LTPhoneNumberField.m
@@ -0,0 +1,31 @@
+//
+//  LTPhoneNumberField.m
+//  PhoneNumberFieldDemo
+//
+//  Created by Colin Regan on 5/8/14.
+//  Copyright (c) 2014 Lua Technologies, LLC. All rights reserved.
+//
+
+#import "LTPhoneNumberField.h"
+
+@implementation LTPhoneNumberField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
