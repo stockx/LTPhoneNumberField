463f149080a2563cc9ad721f15d2282c559b3d03
Colin Regan <colin@redcup.la>
2014-05-08 16:35:32 -0400

    Removes boilerplate

--- PhoneNumberFieldDemo/LTViewController.m
+++ PhoneNumberFieldDemo/LTViewController.m
@@ -8,22 +8,6 @@
 
 #import "LTViewController.h"
 
-@interface LTViewController ()
-
-@end
-
 @implementation LTViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
